3229de1b6dfcb44e878c247b69cc2bf82aab99f5
nelcambo <nelcambo@gmail.com>
2022-08-25 17:05:22 -0500

    Entidad Empleado

--- src/main/java/Entidades/Empresa.java
+++ src/main/java/Entidades/Empresa.java
@@ -1,4 +1,49 @@
 package Entidades;
 
 public class Empresa {
+
+    private String Nombre;
+    private String Direccion;
+    private int Telefono;
+    private String Nit;
+
+    public Empresa(String nombre, String direccion, int telefono, String nit) {
+        this.Nombre = nombre;
+        this.Direccion = direccion;
+        this.Telefono = telefono;
+        this.Nit = nit;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.Nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.Direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.Telefono = telefono;
+    }
+
+    public String getNit() {
+        return Nit;
+    }
+
+    public void setNit(String nit) {
+        this.Nit = nit;
+    }
+
 }
---
+++ src/main/java/Entidades/MovimientoDinero.java
@@ -0,0 +1,8 @@
+package Entidades;
+
+public class MovimientoDinero {
+
+
+
+
+}
