0250801ddccd1329f068752cd4333b3b8f4836d4
rotkill3r <rotkill3r@hotmail.com>
2022-08-25 16:29:11 -0500

    " SpringBoot - Version 0.1 Start Build"

---
+++ src/main/java/com/VProgreSS/demo/VProgressApplication.java
@@ -0,0 +1,13 @@
+package com.VProgreSS.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VProgressApplication.class, args);
+	}
+
+}
