083c15150f05337fb599451eceb07dc3a014f03e
rotkill3r <rotkill3r@hotmail.com>
2022-08-25 15:52:20 -0500

    " Demo Archivo - Commit Main"

---
+++ demo/src/test/java/com/VProgreSS/demo/VProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VProgreSS.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
